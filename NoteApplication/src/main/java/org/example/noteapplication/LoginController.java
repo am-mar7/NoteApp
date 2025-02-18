e46381bd3489770569a0da22c518063320d21a53
am-mar7 <146647513+am-mar7@users.noreply.github.com>
2025-02-18 14:24:03 +0200

    Update LoginController.java

--- NoteApplication/src/main/java/org/example/noteapplication/LoginController.java
+++ NoteApplication/src/main/java/org/example/noteapplication/LoginController.java
@@ -4,29 +4,27 @@ import FileManagement.FileManager;
 import FileManagement.User;
 import NotePackge.Encryption;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
-import javafx.stage.Stage;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable {
     @FXML
     private TextField username_txt;
     @FXML
     private PasswordField password_txt;
     @FXML
     private Label login_Msg;
-
+    HashMap<String,String> map ;
+    HashMap<String, Integer> indices ;
     public void try_Login(ActionEvent e) throws IOException {
         String userName = username_txt.getText();
         String password = Encryption.hash(password_txt.getText());
@@ -39,22 +37,31 @@ public class LoginController {
             login_Msg.setText("Please Enter Username and Password");
             return;
         }
-        int idx = 0;
-        while (idx < data.size()) {
-            User currentUser = data.get(idx);
-            if(currentUser.UserName().equals(userName) && currentUser.Password().equals(password)) {
-                FileManager.setCurrentUser(currentUser);
-//                 do login
-                LocalController lc =  new LocalController();
-                lc.SwitchPage(e,"HomePage.fxml");
-                return;
-            }
-            idx++;
+        if(map.get(userName).equals(password)){
+            // do login
+            FileManager.setCurrentUser((User) data.get(indices.get(userName)));
+            System.out.println((User) data.get(indices.get(userName)));
+            LocalController lc = new LocalController();
+            lc.SwitchPage(e, "HomePage.fxml");
         }
-        login_Msg.setText("Wrong Username or Password ,Please try again");
+
     }
     public void goToSignPage(ActionEvent e) throws IOException {
         LocalController lc =  new LocalController();
         lc.SwitchPage(e,"SignupScreen.fxml");
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        LinkedList<User> data = FileManager.UploadAllData();
+        map = new HashMap<String,String>();
+        indices = new HashMap<String , Integer>();
+        int idx = 0;
+        while(idx < data.size()) {
+            // coping data to a map for better Time complexity
+            map.put(data.get(idx).UserName(),data.get(idx).Password());
+            // mapping the index of each user
+            indices.put(data.get(idx).UserName() , idx);
+            idx++;
+        }
     }
 }
